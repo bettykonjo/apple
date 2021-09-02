006649e02c481cb411cb4e19d2110760504fd762
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2021-09-02 18:34:36 -0400

    uploding apple

---
+++ js/script.js
@@ -0,0 +1,130 @@
+// let tog = $(".ulone")
+// tog.on("click", betty);
+
+// function betty() {
+//     $(".ulone1").toggle()
+// }
+//  $(".footer-links-wrapper h3").click(function () {
+//     if ($(window).width() <= 768) {
+//       $(this).next("ul").slideToggle();
+//       $(this).toggleClass("iconRotator");
+//     }
+ 
+// let tog = $(".footer-links-wrapper h3");
+// tog.on("click",betty);
+
+// function betty() {
+// 	$("ul").toggle()
+// let tog = $("#ulone")
+// tog.on("click", betty);
+
+// function betty() {
+//     $("#ulone1").toggle()
+// }
+
+
+// const shopes = $("#ulone")
+// shopes.on("click",shop);
+
+// function shop(){
+// $("#ulone1").toggle()
+// }
+
+//  const serv = $("#ultwo")
+//   serv.on("click",Services);
+  
+//   function Services(){
+//   $("#ultwo2").toggle()
+//   } 
+  
+//   const acco = $("#ulthree")
+//   acco.on("click",account);
+  
+//   function account(){
+//   $("#ulthree3").toggle()
+//   } 
+
+//   const appsto = $("#ulfour")
+//   appsto.on("click",applestore);
+  
+//   function applestore(){
+//   $("#ulfour4").toggle()
+//   } 
+//   const forbusi = $("ulfive")
+//   forbusi.on("click",forbusiness);
+  
+//   function forbusiness(){
+//   $("#ulfive5").toggle()
+//   } 
+//   const foredu = $("ulsix")
+//   foredu.on("click",foreducation);
+  
+//   function foreducation(){
+//   $("#ulsix6").toggle()
+//   } 
+//   const forheal = $("ulseven")
+//   forhealt.on("click",forhealth);
+  
+//   function forhealth(){
+//   $("#ulseven7").toggle()
+//   } 
+//   const forgov = $("uleight")
+//   forgov.on("click",forgovern);
+  
+//   function forgovern(){
+//   $("#uleight8").toggle()
+//   } 
+//   const appval = $("ulnine")
+//   forgov.on("click",applevalue);
+  
+//   function applevalue(){
+//   $("#ulnine9").toggle()
+//   } 
+//   const aboapp = $("ulten")
+//   aboapp.on("click",aboutapple);
+  
+//   function aboutapple(){
+//   $("#ulten10").toggle()
+//   } 
+  $(".footer-links-wrapper h3").click(function () {
+    if ($(window).width() <= 768) {
+    $(this).next("ul").slideToggle();
+    $(this).toggleClass("iconRotator");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
